86444a12763196905c2c846fa6a9c16e35c2917d
jessklein <jess.klein@live.com>
2018-08-08 18:11:55 -0400

    Tribute page completed as part of the FreeCodeCamp responsive web design curriculum.

---
+++ index.php
@@ -0,0 +1,42 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>FreeCodeCamp - Tribute Project</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/starter-template.css" rel="stylesheet">
+  </head>
+
+  <body>
+    <main id="main" role="main" class="container">
+      <div class="starter-template">
+        <h1 id="title">Ruth Bader Ginsburg</h1>
+        <div id="img-div">
+          <img src="https://upload.wikimedia.org/wikipedia/commons/7/76/Ruth_Bader_Ginsburg_2016_portrait.jpg" id="image" alt="Ruth Bader Ginsburg" class="img-fluid" /><br />
+          <caption id="img-caption">Ruth Bader Ginsburg, Associate Justice of the Supreme Court of the United States</caption>
+        </div>
+        <div id="tribute-info" class="container">
+          <p>From <a href="https://en.wikipedia.org/wiki/Ruth_Bader_Ginsburg" id="tribute-link" target="_blank">Wikipedia</a>:</p>
+          <blockquote>Ruth Bader Ginsburg is an Associate Justice of the Supreme Court of the United States. Ginsburg was appointed by President Bill Clinton and took the oath of office on August 10, 1993. She is the second female justice (after Sandra Day O'Connor) to be confirmed to the court, and one of four female justices to be confirmed (with Sonia Sotomayor and Elena Kagan, who are still serving). Following O'Connor's retirement, and until Sotomayor joined the court, Ginsburg was the only female justice on the Supreme Court. During that time, Ginsburg became more forceful with her dissents, which were noted by legal observers and in popular culture. She is generally viewed as belonging to the liberal wing of the court. Ginsburg has authored notable majority opinions, including United States v. Virginia, Olmstead v. L.C., and Friends of the Earth, Inc. v. Laidlaw Environmental Services, Inc.</blockquote>
+        </div>
+      </div>
+      <div>
+        <p>This <a href="https://learn.freecodecamp.org/responsive-web-design/responsive-web-design-projects/build-a-tribute-page">tribute page</a> completed based on the <a href="https://learn.freecodecamp.org/responsive-web-design/responsive-web-design-projects/">FreeCodeCamp Responsive Web Design curriculum</a>.</p>
+      </div>
+    </main><!-- /.container -->
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
